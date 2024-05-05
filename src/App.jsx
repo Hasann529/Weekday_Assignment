1854af7a6119a952cb0342218acfa94464ab062b
Hasan <hasanraza2905@gmail.com>
2024-05-05 21:37:42 +0530

    Fetching job Description Data from api in App parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.scss'
 import Filters from "./components/Filters";
 import MainBody from "./components/MainBody";
 
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+let offset = -12;
 
 function App() {
+
+  
+  // function for fetching data
+  const fetchData = async () => {
+    try {
+      if (offset >= 0) {
+        const body = JSON.stringify({
+          limit: 12,
+          offset: offset,
+        });
+
+        const requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body,
+        };
+
+        const response = await fetch(
+          "https://api.weekday.technology/adhoc/getSampleJdJSON",
+          requestOptions
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+        const { jdList, totalCount } = result;
+        console.log(jdList);
+
+        
+      }
+      offset = offset + 12;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+  
+    fetchData();
+  }, []);
+
  
 
   return (
