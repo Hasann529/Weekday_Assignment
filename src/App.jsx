ec04e1c3e307b5902c8aeb022ff54b916f090ee5
Hasan <hasanraza2905@gmail.com>
2024-05-05 14:49:27 +0530

    initial component breakup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.scss'
+import Filters from "./components/Filters";
+import MainBody from "./components/MainBody";
+
 
 function App() {
  
 
   return (
     <div className='App'>
-        Start
+        <Filters />
+        <MainBody />
     </div>
   )
 }
---
+++ src/components/MainBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainBody = () => {
+  return (
+    <div className='main-body'>
+      MainBody
+    </div>
+  )
+}
+
+export default MainBody
